878fda82caba171bd89d87e5776452842bb6532f
avaice <dev@cho-ice.xyz>
2024-08-15 04:54:14 +0900

    layout.tsx変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "ASIS-TOBE-Maker 2",
-  description: "Create the ASIS-TOBE gif you want",
-};
+  description: "簡単に比較動画を作成できます",
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
